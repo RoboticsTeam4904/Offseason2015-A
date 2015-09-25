3c222136c1e621ad1939e3a826fbc1a15d34874f
Yoni Lerner <yonilern@gmail.com>
2015-09-25 16:18:14 -0700

    Changed VictorSP to CANTalon

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -4,16 +4,17 @@ package org.usfirst.frc4904.robot;
 import org.usfirst.frc4904.standard.subsystems.chassis.SolenoidShifters;
 import org.usfirst.frc4904.standard.subsystems.chassis.TankDriveShifting;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
+
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.VictorSP;
 
 public class RobotMap {
 	// *** PHYSICAL COMPONENTS *** //
 	// VictorSP-controlled motors
-	public static VictorSP LEFT_WHEEL_MOTOR_B;
-	public static VictorSP RIGHT_WHEEL_MOTOR_A;
-	public static VictorSP RIGHT_WHEEL_MOTOR_B;
-	public static VictorSP LEFT_WHEEL_MOTOR_A;
+	public static CANTalon LEFT_WHEEL_MOTOR_B;
+	public static CANTalon RIGHT_WHEEL_MOTOR_A;
+	public static CANTalon RIGHT_WHEEL_MOTOR_B;
+	public static CANTalon LEFT_WHEEL_MOTOR_A;
 	// Solenoids
 	public static Solenoid LEFT_SOLENOID_UP;
 	public static Solenoid LEFT_SOLENOID_DOWN;
@@ -28,14 +29,14 @@ public class RobotMap {
 	public static TankDriveShifting chassis;
 	
 	public RobotMap() {
-		LEFT_WHEEL_MOTOR_A = new VictorSP(0);
-		LEFT_WHEEL_MOTOR_B = new VictorSP(1);
-		RIGHT_WHEEL_MOTOR_A = new VictorSP(2);
-		RIGHT_WHEEL_MOTOR_B = new VictorSP(3);
-		LEFT_SOLENOID_UP = new Solenoid(0);
-		LEFT_SOLENOID_DOWN = new Solenoid(1);
-		RIGHT_SOLENOID_UP = new Solenoid(2);
-		RIGHT_SOLENOID_DOWN = new Solenoid(3);
+		LEFT_WHEEL_MOTOR_A = new CANTalon(1);
+		LEFT_WHEEL_MOTOR_B = new CANTalon(2);
+		RIGHT_WHEEL_MOTOR_A = new CANTalon(3);
+		RIGHT_WHEEL_MOTOR_B = new CANTalon(4);
+		LEFT_SOLENOID_UP = new Solenoid(6);
+		LEFT_SOLENOID_DOWN = new Solenoid(7);
+		RIGHT_SOLENOID_UP = new Solenoid(6);
+		RIGHT_SOLENOID_DOWN = new Solenoid(7);
 		shifter = new SolenoidShifters(LEFT_SOLENOID_UP, LEFT_SOLENOID_DOWN, RIGHT_SOLENOID_UP, RIGHT_SOLENOID_DOWN);
 		leftWheelA = new Motor("First left wheel", LEFT_WHEEL_MOTOR_A);
 		leftWheelB = new Motor("Second left wheel", LEFT_WHEEL_MOTOR_B);
