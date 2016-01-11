ebe68dbb2f0a8ba7e4ab4f62e09820c6122f5ffc
Carter Turnbaugh <carter@localhost.localdomain>
2016-01-10 16:32:23 -0800

    Fixed solenoid instantiation error.

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -18,10 +18,8 @@ public class RobotMap {
 	public static CANTalon RIGHT_WHEEL_MOTOR_B;
 	public static CANTalon LEFT_WHEEL_MOTOR_A;
 	// Solenoids
-	public static Solenoid LEFT_SOLENOID_UP;
-	public static Solenoid LEFT_SOLENOID_DOWN;
-	public static Solenoid RIGHT_SOLENOID_UP;
-	public static Solenoid RIGHT_SOLENOID_DOWN;
+	public static Solenoid SOLENOID_UP;
+	public static Solenoid SOLENOID_DOWN;
 	public static SolenoidShifters shifter;
 	// Motors
 	public static Motor leftWheelA;
@@ -39,11 +37,9 @@ public class RobotMap {
 		LEFT_WHEEL_MOTOR_B = new CANTalon(2);
 		RIGHT_WHEEL_MOTOR_A = new CANTalon(3);
 		RIGHT_WHEEL_MOTOR_B = new CANTalon(4);
-		LEFT_SOLENOID_UP = new Solenoid(7);
-		LEFT_SOLENOID_DOWN = new Solenoid(6);
-		RIGHT_SOLENOID_UP = new Solenoid(7);
-		RIGHT_SOLENOID_DOWN = new Solenoid(6);
-		shifter = new SolenoidShifters(LEFT_SOLENOID_UP, LEFT_SOLENOID_DOWN, RIGHT_SOLENOID_UP, RIGHT_SOLENOID_DOWN);
+		SOLENOID_UP = new Solenoid(7);
+		SOLENOID_DOWN = new Solenoid(6);
+		shifter = new SolenoidShifters(SOLENOID_UP, SOLENOID_DOWN, SOLENOID_UP, SOLENOID_DOWN);
 		leftWheelA = new Motor("First left wheel", LEFT_WHEEL_MOTOR_A);
 		leftWheelB = new Motor("Second left wheel", LEFT_WHEEL_MOTOR_B);
 		rightWheelA = new Motor("First right wheel", RIGHT_WHEEL_MOTOR_A, true);
