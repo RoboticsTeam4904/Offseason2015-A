a9f5448fa8157e7b352ef325e15fd9555122029a
Carter Turnbaugh <carter@localhost.localdomain>
2016-01-10 17:49:45 -0800

    Removed duplicate Solenoid initializers.

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc4904.robot;
 
 
+import org.usfirst.frc4904.logkitten.LogKitten;
 import org.usfirst.frc4904.standard.custom.sensors.PDP;
 import org.usfirst.frc4904.standard.subsystems.chassis.SolenoidShifters;
 import org.usfirst.frc4904.standard.subsystems.chassis.TankDriveShifting;
