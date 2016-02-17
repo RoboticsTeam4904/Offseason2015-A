d34ad6704be179b5516a3efbb655640bab09268d
Teddy <tobbrau@nuevaschool.org>
2016-02-16 18:52:26 -0800

    only changed degrees per second

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -50,7 +50,7 @@ public class RobotMap {
 			public static double P = 0;// use 0.0055 as the first constant, it would be the "Ideal" value
 			public static double I = 0;
 			public static double D = 0;
-			public static final double maxDegreesPerSecond = 250;
+			public static final double maxDegreesPerSecond = 120;
 		}
 	}
 	
