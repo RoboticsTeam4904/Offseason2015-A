ea2ae52a4f812b1da0ce98fca634a1e0533070ec
Carter Turnbaugh <carturn@nuevaschool.org>
2016-12-13 17:19:22 -0800

    Teensy CAN ID

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -28,25 +28,25 @@ public class RobotMap {
 				public static int rightDriveB = 4;
 			}
 		}
-		
+
 		public static class Pneumatics {
 			public static int solenoidUp = 0;
 			public static int solenoidDown = 1;
 		}
-		
+
 		public static class HumanInput {
 			public static final int joystick = 0;
 			public static final int xboxController = 1;
 		}
-		
+
 		public static class CAN {
 			public static final int backLeds = 0x601;
 			public static final int frontLeds = 0x602;
 			public static final int leftEncoder = 0x611;
-			public static final int rightEncoder = 0x610;
+			public static final int rightEncoder = 0x612;
 		}
 	}
-	
+
 	public static class Constant {
 		public static class HumanInput {
 			public static final double Y_SPEED_SCALE = 1;
@@ -57,7 +57,7 @@ public class RobotMap {
 			public static final double TURN_GAIN = 1;
 			public static final double TURN_EXP = 2;
 		}
-		
+
 		public static class Chassis {
 			public static double TURN_P = 0.02;
 			public static double TURN_I = 0.001;
@@ -67,7 +67,7 @@ public class RobotMap {
 			public static final double WHEEL_PULSES_PER_REVOLUTION = 1024;
 		}
 	}
-	
+
 	public static class Component {
 		public static Motor leftWheel;
 		public static CustomEncoder leftEncoder;
@@ -82,17 +82,17 @@ public class RobotMap {
 		public static OffseasonLEDs backLeds;
 		public static OffseasonLEDs frontLeds;
 	}
-	
+
 	public static class HumanInput {
 		public static class Driver {
 			public static CustomXbox xbox;
 		}
-		
+
 		public static class Operator {
 			public static CustomJoystick stick;
 		}
 	}
-	
+
 	public RobotMap() {
 		Component.pdp = new PDP();
 		Component.shifter = new SolenoidShifters(Port.Pneumatics.solenoidUp, Port.Pneumatics.solenoidDown);
@@ -102,7 +102,7 @@ public class RobotMap {
 		Component.chassisMC.setContinuous(true);
 		Component.leftEncoder = new CANEncoder("LeftEncoder", Port.CAN.leftEncoder, false);
 		Component.rightEncoder = new CANEncoder("RightEncoder", Port.CAN.rightEncoder, false);
-		Component.chassisEncoderMC = new CustomPIDController(0.002, 0.0, -0.004, new EncoderGroup(Component.leftEncoder, Component.rightEncoder));
+		Component.chassisEncoderMC = new CustomPIDController(0.001, 0.0, -0.002, new EncoderGroup(100, Component.leftEncoder, Component.rightEncoder));
 		Component.leftWheel = new Motor("LeftWheel", false, new AccelerationCap(Component.pdp), new CANTalon(Port.Motors.CAN.leftDriveA), new CANTalon(Port.Motors.CAN.leftDriveB));
 		Component.rightWheel = new Motor("RightWheel", false, new AccelerationCap(Component.pdp), new CANTalon(Port.Motors.CAN.rightDriveA), new CANTalon(Port.Motors.CAN.rightDriveB));
 		Component.leftEncoder = new CANEncoder(Port.CAN.leftEncoder);
