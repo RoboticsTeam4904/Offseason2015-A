dc7c783f885122bd3eed960fe1a13a60f856ccb0
Carter Turnbaugh <carturn@nuevaschool.org>
2017-01-04 21:36:29 -0800

    Added fallback test commands (just WTF prints)

--- src/org/usfirst/frc4904/robot/humaninput/drivers/NathanGain.java
+++ src/org/usfirst/frc4904/robot/humaninput/drivers/NathanGain.java
@@ -2,7 +2,9 @@ package org.usfirst.frc4904.robot.humaninput.drivers;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.commands.Kill;
+import org.usfirst.frc4904.standard.commands.KittenCommand;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisIdle;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMotionProfileDistance;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMove;
@@ -16,11 +18,11 @@ public class NathanGain extends Driver {
 	public NathanGain() {
 		super("NathanGain");
 	}
-	
+
 	protected double scaleGain(double input, double gain, double exp) {
 		return Math.pow(input, exp) * gain * Math.signum(input);
 	}
-	
+
 	@Override
 	public void bindCommands() {
 		Command normalDrive = new ChassisMove(RobotMap.Component.chassis, this);
@@ -37,24 +39,24 @@ public class NathanGain extends Driver {
 		RobotMap.HumanInput.Driver.xbox.dPad.right.whenPressed(new ChassisTurnAbsolute(RobotMap.Component.chassis, 270, RobotMap.Component.navx, RobotMap.Component.chassisMC));
 		RobotMap.HumanInput.Driver.xbox.dPad.right.whenReleased(normalDrive);
 		// Auto drive 2 feet
-		RobotMap.HumanInput.Driver.xbox.rb.whenPressed(new ChassisMotionProfileDistance(RobotMap.Component.chassis, 800, RobotMap.Component.chassisEncoderMC, RobotMap.Component.leftEncoder, RobotMap.Component.rightEncoder));
+		RobotMap.HumanInput.Driver.xbox.rb.whenPressed(new ChassisMotionProfileDistance(RobotMap.Component.chassis, 800, RobotMap.Component.chassisEncoderMC, new KittenCommand("Drive Encoder Error", LogKitten.LEVEL_WTF), RobotMap.Component.leftEncoder, RobotMap.Component.rightEncoder));
 		RobotMap.HumanInput.Driver.xbox.rb.whenReleased(normalDrive);
-		RobotMap.HumanInput.Driver.xbox.lb.whenPressed(new ChassisMotionProfileDistance(RobotMap.Component.chassis, -800, RobotMap.Component.chassisEncoderMC, RobotMap.Component.leftEncoder, RobotMap.Component.rightEncoder));
+		RobotMap.HumanInput.Driver.xbox.lb.whenPressed(new ChassisMotionProfileDistance(RobotMap.Component.chassis, -800, RobotMap.Component.chassisEncoderMC, new KittenCommand("Drive Encoder Error", LogKitten.LEVEL_WTF), RobotMap.Component.leftEncoder, RobotMap.Component.rightEncoder));
 		RobotMap.HumanInput.Driver.xbox.lb.whenReleased(normalDrive);
 	}
-	
+
 	@Override
 	public double getX() {
 		return 0;
 	}
-	
+
 	@Override
 	public double getY() {
 		double rawSpeed = RobotMap.HumanInput.Driver.xbox.rt.getX() - RobotMap.HumanInput.Driver.xbox.lt.getX();
 		double speed = scaleGain(rawSpeed, RobotMap.Constant.HumanInput.SPEED_GAIN, RobotMap.Constant.HumanInput.SPEED_EXP) * RobotMap.Constant.HumanInput.Y_SPEED_SCALE;
 		return speed;
 	}
-	
+
 	@Override
 	public double getTurnSpeed() {
 		double rawTurnSpeed = RobotMap.HumanInput.Driver.xbox.leftStick.getX();
